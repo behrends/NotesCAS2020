f13d0a73ae9cd08696d0220767b3fff1319aa7d6
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 15:05:52 +0100

    Screens in eigenen Dateien

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>HOME</Text>
-    </View>
-  );
-}
-
-function Settings() {
-  return (
-    <View style={styles.container}>
-      <Text>SETTINGS</Text>
-    </View>
-  );
-}
+import Home from './screens/Home';
+import Photos from './screens/Photos';
+import Settings from './screens/Settings';
 
 const Tab = createBottomTabNavigator();
 
@@ -27,17 +12,9 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Photos" component={Photos} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
