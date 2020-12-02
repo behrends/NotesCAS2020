deb2bc80f846a0a73d366ba567816f58fe86fb53
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 15:41:32 +0100

    Icons in Tableiste

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Icon from '@expo/vector-icons';
 import Home from './screens/Home';
 import Photos from './screens/Photos';
 import Settings from './screens/Settings';
@@ -14,7 +15,11 @@ const Stack = createStackNavigator();
 
 function HomeStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: 'lavender' },
+      }}
+    >
       <Stack.Screen name="HomeScreen" component={Home} />
       <Stack.Screen name="Details" component={Details} />
       <Stack.Screen name="Edit" component={Edit} />
@@ -25,7 +30,33 @@ function HomeStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let icon;
+
+            if (route.name === 'Home') {
+              icon = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Photos') {
+              icon = focused ? 'image' : 'image-outline';
+            } else if (route.name === 'Settings') {
+              icon = focused ? 'settings' : 'settings-outline';
+            }
+
+            return (
+              <Icon.MaterialCommunityIcons
+                name={icon}
+                size={size}
+                color={color}
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blueviolet',
+          style: { backgroundColor: 'lavender' },
+        }}
+      >
         <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Photos" component={Photos} />
         <Tab.Screen name="Settings" component={Settings} />
