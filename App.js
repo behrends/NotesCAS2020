6b88940c72d796e55f28a649d76673d8e6e9721a
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 15:19:03 +0100

    StackNavigator vorbereitet

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import Photos from './screens/Photos';
 import Settings from './screens/Settings';
+import Details from './screens/Details';
+import Edit from './screens/Edit';
 
 const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={Home} />
+      <Stack.Screen name="Details" component={Details} />
+      <Stack.Screen name="Edit" component={Edit} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Photos" component={Photos} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
