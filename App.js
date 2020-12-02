16d6c2e0fbcf8f81c356504560b0116cb8cc51b1
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 14:36:57 +0100

    Tab-Navigation erstellt

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View style={styles.container}>
+      <Text>SETTINGS</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
